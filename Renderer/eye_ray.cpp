97bf324d9462096a39d355c1ef10b5be6a804827
Egor Feklisov <egor.feklisov@gmail.com>
2024-04-29 22:21:10 +0300

    No gamma correction pls

--- Renderer/eye_ray.cpp
+++ Renderer/eye_ray.cpp
@@ -177,7 +177,7 @@ void MultiRenderer::kernel_RayTrace(uint32_t tidX, const float4* rayPosAndNear,
     break;
     case MULTI_RENDER_MODE_RF:
     {
-      uint3 col = uint3(255 * float3(pow(hit.coords[1], 1.0f / 2.2f), pow(hit.coords[2], 1.0f / 2.2f), pow(hit.coords[3], 1.0f / 2.2f)));
+      uint3 col = uint3(255 * float3(hit.coords[1], hit.coords[2], hit.coords[3]));
       out_color[y * m_width + x] = 0xFF000000 | (col.z<<16) | (col.y<<8) | col.x;
     }
     break;
