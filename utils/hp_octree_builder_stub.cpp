c46d4347bc998442cca81981678bf2f8527a2674
Sammael <albgar-14@yandex.ru>
2024-05-16 21:53:44 +0300

    fixed compilation with -DMODULE_HP_OCTREE=OFF

--- utils/hp_octree_builder_stub.cpp
+++ utils/hp_octree_builder_stub.cpp
@@ -1,6 +1,13 @@
 #include "hp_octree.h"
 
-void HPOctreeBuilder::construct(std::function<float(const float3 &, unsigned thread_idx)> f)
+void HPOctreeBuilder::construct(std::function<float(const float3 &, unsigned thread_idx)> sdf, BuildSettings settings)
 {
-
+  printf("HP Octree module is disabled. Cmake with -DMODULE_HP_OCTREE=ON\n");
 }
+
+void HPOctreeBuilder::construct(const cmesh4::SimpleMesh &mesh, BuildSettings settings)
+{
+  printf("HP Octree module is disabled. Cmake with -DMODULE_HP_OCTREE=ON\n");
+}
+
+  
