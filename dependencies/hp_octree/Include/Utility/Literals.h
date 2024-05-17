32927734bec02450063379fe558d0de6a9cea008
Sammael <albgar-14@yandex.ru>
2024-05-17 10:19:02 +0300

    added hp-octree library

---
+++ dependencies/hp_octree/Include/Utility/Literals.h
@@ -0,0 +1,14 @@
+#pragma once
+
+typedef double                 f64;
+typedef float                  f32;
+typedef int                    i16;
+typedef long int               i32;
+typedef unsigned char          u8;
+typedef unsigned short         u16;
+typedef unsigned int           u32;
+typedef long long unsigned int u64;
+typedef size_t                 usize;
+
+constexpr usize PATH_MAX_LEN = 1024;
+constexpr f32   EPSILON_F32  = 0.000001f;
