a841bb21ed602de586a1b06e06c17aa6bc916a6b
Sammael <albgar-14@yandex.ru>
2024-06-07 11:42:05 +0300

    minor changes in benchmark script

--- main_benchmark.sh
+++ main_benchmark.sh
@@ -1,33 +1,34 @@
 #!/bin/bash
-#./render_app -benchmark dragon build >> ./saves/dragon/auto_results.txt
+
+./render_app -benchmark $3 build >> ./saves/$3/auto_results.txt
 
 bash scripts/slicer_preprocess_grid_only.sh $1 $2
 bash scripts/slicer_build_shaders.sh $1 $2
 make -j15
-./render_app -benchmark dragon sdf_grid >> ./saves/dragon/auto_results.txt
+./render_app -benchmark $3 sdf_grid >> ./saves/$3/auto_results.txt
 
 bash scripts/slicer_preprocess_octree_only.sh $1 $2
 bash scripts/slicer_build_shaders.sh $1 $2
 make -j15
-./render_app -benchmark dragon sdf_octree >> ./saves/dragon/auto_results.txt
+./render_app -benchmark $3 sdf_octree >> ./saves/$3/auto_results.txt
 
 bash scripts/slicer_preprocess_framed_only.sh $1 $2
 bash scripts/slicer_build_shaders.sh $1 $2
 make -j15
-./render_app -benchmark dragon sdf_frame_octree >> ./saves/dragon/auto_results.txt
+./render_app -benchmark $3 sdf_frame_octree >> ./saves/$3/auto_results.txt
 
 bash scripts/slicer_preprocess_sbs_only.sh $1 $2
 bash scripts/slicer_build_shaders.sh $1 $2
 make -j15
-./render_app -benchmark dragon sdf_SBS-2-1 >> ./saves/dragon/auto_results.txt
-./render_app -benchmark dragon sdf_SBS-2-2 >> ./saves/dragon/auto_results.txt
+./render_app -benchmark $3 sdf_SBS-2-1 >> ./saves/$3/auto_results.txt
+./render_app -benchmark $3 sdf_SBS-2-2 >> ./saves/$3/auto_results.txt
 
 bash scripts/slicer_preprocess_hp_only.sh $1 $2
 bash scripts/slicer_build_shaders.sh $1 $2
 make -j15
-./render_app -benchmark dragon sdf_hp_octree >> ./saves/dragon/auto_results.txt
+./render_app -benchmark $3 sdf_hp_octree >> ./saves/$3/auto_results.txt
 
 bash scripts/slicer_preprocess_svs_only.sh $1 $2
 bash scripts/slicer_build_shaders.sh $1 $2
 make -j15
-./render_app -benchmark dragon sdf_SVS >> ./saves/dragon/auto_results.txt
+./render_app -benchmark $3 sdf_SVS >> ./saves/$3/auto_results.txt
