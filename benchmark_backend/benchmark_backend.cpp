efdd224cdf550ffee1df20c23b639f971ff39b40
sadjames <sergey.nikitin2013@gmail.com>
2024-12-02 00:25:55 +0300

    fix bad linking in cmakelists, add slooow adaptive sbs

--- benchmark_backend/benchmark_backend.cpp
+++ benchmark_backend/benchmark_backend.cpp
@@ -76,7 +76,7 @@ namespace BenchmarkBackend
             float t = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / 1000.f;
             calcMetrics(min_time, max_time, average_time, t);
 
-            std::string img_name = "benchmark/saves/" + type + "_" + std::to_string(camera) + ".bmp";
+            std::string img_name = "benchmark/saves/" + backend + "_" + type + "_" + std::to_string(camera) + ".bmp";
             LiteImage::SaveImage<uint32_t>(img_name.c_str(), image);
 
             //  calculate metrics
@@ -91,114 +91,6 @@ namespace BenchmarkBackend
         f << model << ", " << backend << ", " << renderer << ", " << type << ", " << lod << ", " << memory << ", " << min_time << ", " << max_time << ", " << average_time << ", " << min_psnr << ", " << max_psnr << ", " << average_psnr << ", " << min_flip << ", " << max_flip << ", " << average_flip << std::endl;
         f.close();
     }
-
-        // for (const auto &lod : config.lods)
-        // {
-        //     auto mesh = cmesh4::LoadMeshFromVSGF(model.c_str());
-        //     cmesh4::rescale_mesh(mesh, float3(-0.95, -0.95, -0.95), float3(0.95, 0.95, 0.95));
-
-        //     MultiRenderPreset preset = createPreset(renderer, config.spp);
-            
-        //     auto pRender = CreateMultiRenderer(getDevice(backend));
-        //     pRender->SetPreset(preset);
-
-        //     float min_time = 1e4, max_time = -1, average_time = 0;
-        //     float memory = 0;
-        //     float min_psnr = 1000, max_psnr = -1, average_psnr = 0;
-        //     float min_flip = 1000, max_flip = -1, average_flip = 0;
-
-        //     if (lod == "low")
-        //     {
-
-        //     }
-        //     else if (lod == "mid")
-        //     {
-
-        //     }
-        //     else if (lod == "high")
-        //     {
-        //     //  Load model into chosen structure
-        //     if (repr_type == "MESH")
-        //     {
-        //         memory = sizeof(float) * (float)(mesh.IndicesNum() + mesh.VerticesNum()) / 1024.f / 1024.f;
-        //         pRender->SetScene(mesh);
-        //     }
-        //     else if (repr_type == "SDF_GRID")
-        //     {
-        //         auto grid = sdf_converter::create_sdf_grid(GridSettings(64), mesh);
-        //         memory = sizeof(float) * (float)grid.data.size() / 1024.f / 1024.f;
-
-        //         pRender->SetScene(grid);
-        //     }
-        //     else if (repr_type == "SDF_SVS")
-        //     {
-        //         SparseOctreeSettings settings(SparseOctreeBuildType::DEFAULT, 7);
-        //         std::vector<SdfSVSNode> frame_nodes = sdf_converter::create_sdf_SVS(settings, mesh);
-        //         memory = sizeof(SdfSVSNode) * frame_nodes.size() / 1024.f / 1024.f;
-
-        //         pRender->SetScene(frame_nodes);
-        //     }
-        //     else if (repr_type == "SDF_SBS")
-        //     {
-        //         SdfSBSHeader header;
-        //         header.brick_size = 3;
-        //         header.brick_pad = 0;
-        //         header.bytes_per_value = 1;
-        //         header.aux_data = SDF_SBS_NODE_LAYOUT_DX;
-
-        //         auto sbs = sdf_converter::create_sdf_SBS(SparseOctreeSettings(SparseOctreeBuildType::DEFAULT, 5), header, mesh);
-
-        //         pRender->SetScene(sbs);
-        //     }
-        //     else if (repr_type == "SDF_SBS_ADAPT")
-        //     {
-        //         std::vector<MeshBVH> bvh(1);
-        //         for (unsigned i = 0; i < 1; i++)
-        //         bvh[i].init(mesh);
-        //         auto real_sdf = [&](const float3 &p, unsigned idx) -> float 
-        //         { return bvh[idx].get_signed_distance(p);};
-        //         //  Неработающая ..... 
-        //         SdfSBSAdapt sbs_adapt = sdf_converter::greed_sbs_adapt(real_sdf, 5);
-
-        //         pRender->SetScene(sbs_adapt);
-        //     }
-        //     }
-
-        //     for (int camera = 0; camera < config.cameras; camera++)
-        //     {
-        //     const float dist = 2;
-        //     float angle = 2.0f * LiteMath::M_PI * camera / (float)config.cameras;
-        //     const float3 pos = dist*float3(sin(angle), 0, cos(angle));
-
-        //     LiteImage::Image2D<uint32_t> image(config.width, config.height);
-
-        //     auto t1 = std::chrono::steady_clock::now();
-        //     render(image, pRender, pos, float3(0,0,0), float3(0,1,0), preset, 1);
-        //     auto t2 = std::chrono::steady_clock::now();
-
-        //     //  Time calculation
-        //     float t = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / 1000.f;
-        //     calcMetrics(min_time, max_time, average_time, t);
-            
-        //     if (repr_type == "MESH")
-        //     {
-        //         ref_images.push_back(image);
-        //     }
-
-        //     std::string img_name = "benchmark/saves/" + repr_type + "_" + std::to_string(camera) + ".bmp";
-        //     LiteImage::SaveImage<uint32_t>(img_name.c_str(), image);
-
-        //     //  calculate metrics
-        //     calcMetrics(min_psnr, max_psnr, average_psnr, image_metrics::PSNR(image, ref_images[camera]));
-        //     calcMetrics(min_flip, max_flip, average_flip, image_metrics::FLIP(image, ref_images[camera]));
-        //     }
-
-        //     average_time /= (float)config.cameras;
-        //     average_psnr /= (float)config.cameras;
-        //     average_flip /= (float)config.cameras;
-
-        //     f << model << ", " << backend << ", " << renderer << ", " << repr_type << ", " << lod << ", " << memory << ", " << min_time << ", " << max_time << ", " << average_time << ", " << min_psnr << ", " << max_psnr << ", " << average_psnr << ", " << min_flip << ", " << max_flip << ", " << average_flip << std::endl;
-        // }
 
     void 
     getInfoGrid(const std::string &model, const std::string &backend, const std::string &renderer, 
@@ -257,7 +149,7 @@ namespace BenchmarkBackend
             float t = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / 1000.f;
             calcMetrics(min_time, max_time, average_time, t);
 
-            std::string img_name = "benchmark/saves/" + type + "_" + std::to_string(camera) + ".bmp";
+            std::string img_name = "benchmark/saves/" + backend + "_" + type + "_" + std::to_string(camera) + ".bmp";
             LiteImage::SaveImage<uint32_t>(img_name.c_str(), image);
 
             //  calculate metrics
@@ -332,7 +224,7 @@ namespace BenchmarkBackend
             float t = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / 1000.f;
             calcMetrics(min_time, max_time, average_time, t);
 
-            std::string img_name = "benchmark/saves/" + type + "_" + std::to_string(camera) + ".bmp";
+            std::string img_name = "benchmark/saves/" + backend + "_" + type + "_" + std::to_string(camera) + ".bmp";
             LiteImage::SaveImage<uint32_t>(img_name.c_str(), image);
 
             //  calculate metrics
@@ -417,7 +309,7 @@ namespace BenchmarkBackend
             float t = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / 1000.f;
             calcMetrics(min_time, max_time, average_time, t);
 
-            std::string img_name = "benchmark/saves/" + type + "_" + std::to_string(camera) + ".bmp";
+            std::string img_name = "benchmark/saves/" + backend + "_" + type + "_" + std::to_string(camera) + ".bmp";
             LiteImage::SaveImage<uint32_t>(img_name.c_str(), image);
 
             //  calculate metrics
@@ -438,7 +330,83 @@ namespace BenchmarkBackend
     getInfoAdaptSBS(const std::string &model, const std::string &backend, const std::string &renderer, 
         const std::string &type, const std::string &lod, const int width, const int height, const int spp, const int cameras)
     {
+        float min_time = 1e4, max_time = -1, average_time = 0;
+        float memory = 0;
+        float min_psnr = 1000, max_psnr = -1, average_psnr = 0;
+        float min_flip = 1000, max_flip = -1, average_flip = 0;
 
+        std::fstream f;
+        f.open("benchmark/results/results.csv", std::ios::app);
+
+        auto mesh = cmesh4::LoadMeshFromVSGF(model.c_str());
+        cmesh4::rescale_mesh(mesh, float3(-0.95, -0.95, -0.95), float3(0.95, 0.95, 0.95));
+
+        std::vector<MeshBVH> bvh(1);
+        for (unsigned i = 0; i < 1; i++)
+            bvh[i].init(mesh);
+        auto real_sdf = [&](const float3 &p, unsigned idx) -> float 
+        { return bvh[idx].get_signed_distance(p);};
+        
+        int depth = 1;
+
+        if (lod == "medium")
+        {
+            depth = 3;
+        }
+        else
+        {
+            depth = 5;
+        }
+
+        SdfSBSAdapt sbs_adapt = sdf_converter::greed_sbs_adapt(real_sdf, depth);
+
+        MultiRenderPreset preset = createPreset(renderer, spp);
+
+        auto pRender_mesh = CreateMultiRenderer(getDevice(backend));
+        pRender_mesh->SetPreset(preset);
+        pRender_mesh->SetScene(mesh);
+
+        auto pRender_sbs_adapt = CreateMultiRenderer(getDevice(backend));
+        pRender_sbs_adapt->SetPreset(preset);
+        pRender_sbs_adapt->SetScene(sbs_adapt);
+
+        //  dont know how to get structure memory size
+        memory = 0;
+        // memory /= 1024.0f * 1024.0f;
+
+        for (int camera = 0; camera < cameras; camera++)
+        {
+            const float dist = 2;
+            float angle = 2.0f * LiteMath::M_PI * camera / (float)cameras;
+            const float3 pos = dist*float3(sin(angle), 0, cos(angle));
+
+            LiteImage::Image2D<uint32_t> image(width, height), ref_image(width, height);
+            
+            render(ref_image, pRender_mesh, pos, float3(0,0,0), float3(0,1,0), preset, 1);
+
+            auto t1 = std::chrono::steady_clock::now();
+            render(image, pRender_sbs_adapt, pos, float3(0,0,0), float3(0,1,0), preset, 1);
+            auto t2 = std::chrono::steady_clock::now();
+
+            //  Time calculation
+            float t = std::chrono::duration_cast<std::chrono::milliseconds>(t2 - t1).count() / 1000.f;
+            calcMetrics(min_time, max_time, average_time, t);
+
+            std::string img_name = "benchmark/saves/" + backend + "_" + type + "_" + std::to_string(camera) + ".bmp";
+            LiteImage::SaveImage<uint32_t>(img_name.c_str(), image);
+
+            //  calculate metrics
+            calcMetrics(min_psnr, max_psnr, average_psnr, image_metrics::PSNR(image, ref_image));
+            calcMetrics(min_flip, max_flip, average_flip, image_metrics::FLIP(image, ref_image));
+        }
+
+        average_time /= (float)cameras;
+        average_psnr /= (float)cameras;
+        average_flip /= (float)cameras;
+
+        f << model << ", " << backend << ", " << renderer << ", " << type << ", " << lod << ", " << memory << ", " 
+          << min_time << ", " << max_time << ", " << average_time << ", " << min_psnr << ", " << max_psnr 
+          << ", " << average_psnr << ", " << min_flip << ", " << max_flip << ", " << average_flip << std::endl;
     }
 
     MultiRenderPreset 
