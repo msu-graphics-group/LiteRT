315dc0ad18e859f656ae974809a759bb4afa25a6
Sammael <albgar-14@yandex.ru>
2024-12-15 20:06:41 +0300

    minor fixes to visualizer

--- scripts/plot_turbine.py
+++ scripts/plot_turbine.py
@@ -1,6 +1,6 @@
 import sys
-if len(sys.argv) == 1:
-    print('Usage: python3 plot_turbine.py input_file.csv [save-format]')
+if len(sys.argv) < 4:
+    print('Usage: python3 plot_turbine.py input_file.csv model_name save-format')
     print('save-format is optional, can be "png", "pdf", "svg", etc')
     exit(1)
 else:
@@ -12,16 +12,15 @@ import numpy as np
 import pandas as pd
 from matplotlib.ticker import LogLocator, FormatStrFormatter, ScalarFormatter
 
-format = None
-if len(sys.argv) > 2:
-    format = sys.argv[2]
+target_mode_name = sys.argv[2]
+format = sys.argv[3]
 
 df = pd.read_csv(csv_path)
 # Strip whitespace
-df = df.map(lambda x: x.strip() if isinstance(x, str) else x)
+#df = df.map(lambda x: x.strip() if isinstance(x, str) else x)
 df.columns = df.columns.str.strip()
 
-df = df[(df.model_name == 'Turbine-obj') & (df['type'] != 'MESH')]
+df = df[(df.model_name == target_mode_name) & (df['type'] != 'MESH')]
 
 # Здесь можно поставить font='Times New Roman' или любой другой
 sns.set_theme(style='whitegrid', font_scale=1.3)
@@ -34,15 +33,15 @@ def normal_plot():
         data=df,
         x='model_size(Mb)',
         y='psnr_average',
-        hue='type',
+        hue='tag',
         marker='o',
         linewidth=2,
         palette='Set1',
     )
-    ax.set_title('Зависимость PSNR от размера модели')
-    ax.legend(title='Тип модели', loc='lower right')
-    ax.set_xlabel('Размер модели (МБ)')
-    ax.set_ylabel('Среднее PSNR')
+    ax.set_title('Quality versus model size for different model types')
+    ax.legend(title='Model type', loc='lower right')
+    ax.set_xlabel('Model size (Mb)')
+    ax.set_ylabel('PSNR')
 
     ax.minorticks_on()
     return ax
@@ -77,8 +76,7 @@ ax.grid(True, 'minor', alpha=0.6, linestyle='--', linewidth=0.6)
 ax.set_xscale('log', base=2)
 
 # Remove scientific notation
-major_formatter = ScalarFormatter()
-major_formatter.set_scientific(False)
+major_formatter = FormatStrFormatter("%.2f")
 ax.xaxis.set_major_formatter(major_formatter)
 
 draw_plot('output2')
@@ -100,8 +98,7 @@ ax.xaxis.set_minor_locator(minor_locator)
 ax.xaxis.set_minor_formatter(FormatStrFormatter("%.1f"))
 
 # Remove scientific notation
-major_formatter = ScalarFormatter()
-major_formatter.set_scientific(False)
+major_formatter = FormatStrFormatter("%.2f")
 ax.xaxis.set_major_formatter(major_formatter)
 
 draw_plot('output3')
